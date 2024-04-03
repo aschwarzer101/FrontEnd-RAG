854891d8fe4562aa0552c28423e87b93d95b342c
aschwarzer101 <124056235+aschwarzer101@users.noreply.github.com>
2024-04-02 21:12:25 -0400

    rerouting app.tsx to Playground first

--- aws-genai-llm-chatbot-main/lib/user-interface/react-app/src/app.tsx
+++ aws-genai-llm-chatbot-main/lib/user-interface/react-app/src/app.tsx
@@ -8,23 +8,16 @@ import {
 } from "react-router-dom";
 import { AppContext } from "./common/app-context";
 import GlobalHeader from "./components/global-header";
-import Models from "./pages/chatbot/models/models";
-import MultiChatPlayground from "./pages/chatbot/playground/multi-chat-playground";
 import Playground from "./pages/chatbot/playground/playground";
 import NotFound from "./pages/not-found";
 import AddData from "./pages/rag/add-data/add-data";
 import CreateWorkspace from "./pages/rag/create-workspace/create-workspace";
-import CrossEncoders from "./pages/rag/cross-encoders/cross-encoders";
 import Dashboard from "./pages/rag/dashboard/dashboard";
-import Embeddings from "./pages/rag/embeddings/embeddings";
-import Engines from "./pages/rag/engines/engines";
-import SemanticSearch from "./pages/rag/semantic-search/semantic-search";
 import RssFeed from "./pages/rag/workspace/rss-feed";
 import WorkspacePane from "./pages/rag/workspace/workspace";
 import Workspaces from "./pages/rag/workspaces/workspaces";
-import Welcome from "./pages/welcome";
 import "./styles/app.scss";
-import SessionPage from "./pages/chatbot/sessions/sessions";
+// import SessionPage from "./pages/chatbot/sessions/sessions";
 
 function App() {
   const appContext = useContext(AppContext);
@@ -37,20 +30,14 @@ function App() {
         <div style={{ height: "56px", backgroundColor: "#000716" }}>&nbsp;</div>
         <div>
           <Routes>
-            <Route index path="/" element={<Welcome />} />
+            <Route index path="/" element={<Playground />} />
             <Route path="/chatbot" element={<Outlet />}>
               <Route path="playground" element={<Playground />} />
               <Route path="playground/:sessionId" element={<Playground />} />
-              <Route path="sessions" element={<SessionPage />} />
-              <Route path="multichat" element={<MultiChatPlayground />} />
-              <Route path="models" element={<Models />} />
+              {/* <Route path="sessions" element={<SessionPage />} /> if we want history*/}
             </Route>
             <Route path="/rag" element={<Outlet />}>
               <Route path="" element={<Dashboard />} />
-              <Route path="engines" element={<Engines />} />
-              <Route path="embeddings" element={<Embeddings />} />
-              <Route path="cross-encoders" element={<CrossEncoders />} />
-              <Route path="semantic-search" element={<SemanticSearch />} />
               <Route path="workspaces" element={<Workspaces />} />
               <Route path="workspaces/create" element={<CreateWorkspace />} />
               <Route
